ae7dd4687c96960ea3bb83315f1ccf42535a782e
phuonghn <ngocphuong1999pt@gmail.com>
2023-03-27 22:20:38 +0700

    source

--- src/app/pages/welcome/welcome.component.ts
+++ src/app/pages/welcome/welcome.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-welcome',
@@ -6,14 +6,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./welcome.component.css']
 })
 export class WelcomeComponent implements OnInit {
-  sourceFile: any;
+  lstFileVN: string[] = [];
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
 
   onChooseFile() {
-    console.log(this.sourceFile);
+    console.log(this.decodeUtf8('Trang bá» Accio cho Bá» phÃ©p thuáº­t cá»§a báº¡n'))
+  }
+
+  changeFile(event: Event) {
+    const reader = new FileReader();
+    reader.onload = function (e) {
+      const text = e?.target?.result;
+      // @ts-ignore
+      const lines = text.split('\n');
+
+      for (let line = 0; line < lines.length; line++) {
+        console.log(lines[line])
+      }
+    };
+    // @ts-ignore
+    reader.readAsBinaryString(event?.target?.files[0]);
+  }
+
+  encodeUtf8(s: any) {
+    return unescape(encodeURIComponent(s));
+  }
+
+  decodeUtf8(s: any) {
+    return decodeURIComponent(escape(s));
   }
 }
--- src/app/pages/welcome/welcome.module.ts
+++ src/app/pages/welcome/welcome.module.ts
@@ -6,10 +6,11 @@ import { WelcomeComponent } from './welcome.component';
 import {NzInputModule} from "ng-zorro-antd/input";
 import {FormsModule} from "@angular/forms";
 import {NzButtonModule} from "ng-zorro-antd/button";
+import {NzUploadModule} from "ng-zorro-antd/upload";
 
 
 @NgModule({
-  imports: [WelcomeRoutingModule, NzInputModule, FormsModule, NzButtonModule],
+  imports: [WelcomeRoutingModule, NzInputModule, FormsModule, NzButtonModule, NzUploadModule],
   declarations: [WelcomeComponent],
   exports: [WelcomeComponent]
 })
